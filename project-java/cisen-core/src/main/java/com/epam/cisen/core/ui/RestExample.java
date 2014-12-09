3ce49e042be70d8b87956638a5672ac04b008b56
Vladislav <Bilyk_Vladyslav@epam.com>
2014-12-09 18:11:49 +0200

    Update Rest Examples. Add post example.

--- project-java/cisen-core/src/main/java/com/epam/cisen/core/ui/RestExample.java
+++ project-java/cisen-core/src/main/java/com/epam/cisen/core/ui/RestExample.java
@@ -1,24 +1,63 @@
 package com.epam.cisen.core.ui;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MultivaluedMap;
 
+import com.epam.cisen.core.api.MongoDBService;
+import com.epam.cisen.core.api.dto.Constants;
+import com.mongodb.DBCursor;
+import com.mongodb.DBObject;
+import com.mongodb.util.JSON;
 import org.apache.felix.scr.annotations.Component;
+import org.apache.felix.scr.annotations.Reference;
 import org.apache.felix.scr.annotations.Service;
+
+import java.util.List;
+import java.util.Objects;
+
 
 /**
  * I am available by "/services/status" path.
- *
  */
 @Component
 @Service(RestExample.class)
 @Path("/status")
 public class RestExample {
 
+
+    @Reference
+    protected MongoDBService mongoDBService;
+
+
     @GET
     @Produces("application/json")
     public SimpleJsonResponse getStatus() {
         return new SimpleJsonResponse("myName", "myValue");
+    }
+
+    @GET
+    @Path("/test")
+    @Produces("application/json")
+    public String getDBObject() {
+        DBCursor cursor = null;
+        try {
+            cursor = mongoDBService.getDBCollection(Constants.DB.CI_PLUGINS).find();
+            StringBuilder result = new StringBuilder();
+            for (DBObject object : cursor.toArray()) {
+                result.append(object.toString());
+            }
+            return result.toString();
+        } finally {
+            if (cursor != null) {
+                cursor.close();
+            }
+        }
+    }
+
+    @POST
+    @Path("/job")
+    @Consumes("application/x-www-form-urlencoded")
+    public void set(MultivaluedMap<String, String> formParams) {
+        System.out.println(formParams);
     }
 }
