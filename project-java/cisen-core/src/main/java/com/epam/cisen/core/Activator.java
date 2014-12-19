6cb91fca7d4ad02fde244837ce50bcad2d3182e4
Andrew Manuev <andrii_manuiev@epam.com>
2014-12-19 16:44:01 +0200

    Add auto copy of WEB resources inside CORE module. Add resource provider to output resources through Jetty.

--- project-java/cisen-core/src/main/java/com/epam/cisen/core/Activator.java
+++ project-java/cisen-core/src/main/java/com/epam/cisen/core/Activator.java
@@ -2,6 +2,9 @@ package com.epam.cisen.core;
 
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceReference;
+import org.osgi.service.http.HttpService;
+import org.osgi.util.tracker.ServiceTracker;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -9,14 +12,51 @@ public class Activator implements BundleActivator {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(Activator.class);
 
+    public static final String ALIAS = "/";
+    public static final String RESOURCE_FOLDER = "/webapp";
+
+    private ServiceTracker httpServiceTracker;
+
     @Override
     public void start(BundleContext bundleContext) throws Exception {
         LOGGER.info("Cisen Core started");
+
+        httpServiceTracker = getResourceServiceTracker(bundleContext);
+        httpServiceTracker.open();
+
     }
 
     @Override
     public void stop(BundleContext bundleContext) throws Exception {
         LOGGER.info("Cisen Core stopped");
+
+        httpServiceTracker.close();
+    }
+
+    @SuppressWarnings("unchecked")
+    private ServiceTracker getResourceServiceTracker(final BundleContext bundleContext) {
+
+        return new ServiceTracker(bundleContext, HttpService.class.getName(), null) {
+            public void removedService(ServiceReference reference, Object service) {
+                LOGGER.info("HTTP service is no longer available, unregister our resources...");
+                try {
+                    ((HttpService) service).unregister(ALIAS);
+                } catch (IllegalArgumentException ex) {
+                    LOGGER.error("Ignore; servlet registration probably failed earlier on...");
+                }
+            }
+
+            public Object addingService(ServiceReference reference) {
+                LOGGER.info("HTTP service is available, register our resources...");
+                HttpService httpService = (HttpService) this.context.getService(reference);
+                try {
+                    httpService.registerResources(ALIAS, RESOURCE_FOLDER, null);
+                } catch (Exception ex) {
+                    LOGGER.info("Fail to register resources...", ex);
+                }
+                return httpService;
+            }
+        };
     }
 
 }
